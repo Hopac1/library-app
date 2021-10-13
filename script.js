c7241855f7aa0232eb195794566783fe6e36371b
Hopac1 <chris1101@hotmail.co.uk>
2021-10-13 17:30:18 +0100

    allow user to add a book if it hasn't already been added

---
+++ script.js
@@ -0,0 +1,43 @@
+const libraryContainer = document.querySelector(".library-container")
+const newBookBtn = document.querySelector(".new-book-btn")
+let myLibrary = [];
+
+class Book {
+    constructor (
+        title = "",
+        author = "",
+        pages = "",
+        isRead = false
+    ){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.isRead = isRead;
+    }
+}
+
+function addBookToLibrary(newBook) {
+    if (!inLibrary(newBook)) {
+        myLibrary.push(newBook);
+        displayBooks()
+    }
+}
+
+function inLibrary(newBook) {
+    return myLibrary.some(book => book.title === newBook.title);
+}
+
+function displayBooks() {
+    myLibrary.forEach(book => {
+        let currentBook = document.createElement("div");
+        currentBook.textContent += `${book.title} by ${book.author}, ${book.pages} pages`;
+        libraryContainer.appendChild(currentBook);
+    })
+}
+
+
+// Event Listeners
+newBookBtn.addEventListener("click", () => {
+    const newBook = new Book("Three-Ring Circus", "Jeff Pearlman", "409", false);
+    addBookToLibrary(newBook);
+});
